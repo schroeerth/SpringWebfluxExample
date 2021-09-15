5cf6776cc46d155f6a7ea3a0cfd20f6189305ae6
schroeer <thomas.schroeer@adesso.de>
2021-09-15 18:57:11 +0200

    initialized project

---
+++ src/test/java/de/schroeerth/springwebfluxexample/SpringWebfluxExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.schroeerth.springwebfluxexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
